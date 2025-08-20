008c5bbbb9b50f571b908d8de9e85fe0bc9e01ac
CHODISETTI PADMA TEJASRI <chodisettipadmatejasri@gmail.com>
2025-08-20 19:43:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
+
+
+
+function shopNow() {
+  alert("Redirecting to our exclusive gold collection...");
+  // You can redirect to another page with: window.location.href = 'shop.html';
+}
+
+
+
+
+
+
+function orderNow(product) {
+  alert(`Order placed for: ${product}\nWe will contact you soon!`);
+}
+
+//
+function navigateTo(page) {
+  window.location.href = page;
+}
+
+
+//add//
+
+
+
+
+
+
+
+
